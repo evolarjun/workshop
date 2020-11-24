401b2ca21c3f82d4917a3b475995360ea3dd06c5
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2020-11-24 08:45:51 -0500

    Added scripts and data for project 1 step 4

---
+++ project1/spreadsheet2tidy.pl
@@ -0,0 +1,23 @@
+#!/bin/env perl
+use strict;
+use warnings;
+use GTB::File qw(Open);
+
+my $in = Open('spreadsheet-phenotypes.tab2');
+
+my $h = <$in>;
+chomp($h);
+my @h = split(/\t/, $h);
+
+my @data;
+
+while (<$in>) {
+    chomp;
+    my @F = split(/\t/);
+    for (my $i = 1; $i <= $#F; $i++) {
+        my $drug = $h[$i];
+        print "$F[0]\t$drug\t$F[$i]\n";
+    }
+}
+
+
